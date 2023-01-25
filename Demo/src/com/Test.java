f007b98830c723cbc27a55c5cb1ac4dbd71a3082
Shivanisarnaik <123478628+Shivanisarnaik@users.noreply.github.com>
2023-01-25 18:53:12 +0530

    Update Test.java
    Student class file

--- Demo/src/com/Test.java
+++ Demo/src/com/Test.java
@@ -3,6 +3,13 @@ package com;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("Test class......");
+		
+		Student student=new Student();
+		student.setId(1);
+		student.setName("Sita");
+		student.setAddress("pune");
+		
+		System.out.println("Student :" +student);
 	}
 
 }
